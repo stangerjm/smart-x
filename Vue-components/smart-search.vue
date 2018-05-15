35f2477fb09fd2bb63b07576ac209230f518b998
Stanger <JMST225@patrol.lcl>
2018-05-15 16:41:56 -0700

    Added documentation for smart-search component

--- Vue-components/smart-search.vue
+++ Vue-components/smart-search.vue
@@ -7,24 +7,26 @@
             </div>
             <div class="smart-search--headingSegment">
                 <div class="smart-search--toggleContainer">
-                    <div :class="['smart-search--fieldContainer', isSearchBtn ? 'is-hidden' : '']">
+                    <div :class="['smart-search--fieldContainer', isHidden ? 'is-hidden' : '']">
                         <bit-input
-                                :is-disabled="isSearchBtn"
+                                :is-disabled="isHidden"
                                 input-type="text"
                                 label-text="Find by ID/Name"
                                 input-name="search-field">
                         </bit-input>
                         <bit-input
-                                :is-disabled="isSearchBtn"
+                                :is-disabled="isHidden"
                                 input-type="number"
                                 label-text="Number of Results Per Page"
                                 input-name="page-number">
                         </bit-input>
-                        <bit-btn :disabled="isSearchBtn" type="submit" btn-style="search">Search</bit-btn>
+                        <bit-btn :disabled="isHidden" type="submit" btn-style="search">Search</bit-btn>
                     </div>
                 </div>
-                <bit-btn class="smart-search--btnSearch" :btn-style="searchStyle" v-show="isSearchBtn" @click.native="toggle()"></bit-btn>
-                <bit-btn class="smart-search--btnExit" :btn-style="exitStyle" v-show="!isSearchBtn" @click.native="toggle()"></bit-btn>
+                <bit-btn class="smart-search--btnSearch" btn-style="plainSearch" v-show="isHidden"
+                         @click.native="toggle()"></bit-btn>
+                <bit-btn class="smart-search--btnExit" btn-style="plainExit" v-show="!isHidden"
+                         @click.native="toggle()"></bit-btn>
             </div>
         </div>
     </form>
@@ -33,80 +35,109 @@
 <script>
   import bitBtn from './bit-btn'
   import bitInput from './bit-input'
-    export default {
-      name: "smart-search",
-      components: {
-        bitBtn,
-        bitInput
-      },
-      data() {
-        return {
-          isSearchBtn: true,
-          searchContainerClass: "smart-search--fieldContainer is-hidden",
-          searchStyle: 'plainSearch',
-          exitStyle: 'plainExit'
-        }
-      },
-      props: {
-          action: {
-            type: String,
-            required: true
-          },
-          method: {
-            type: String,
-            required: true
-          },
-          formTitle: {
-            type: String,
-            required: true
-          }
-      },
-      methods: {
-        toggle: function() {
-          if (this.isSearchBtn) {
-            this.isSearchBtn = false;
-            this.resize();
-          } else {
-            this.isSearchBtn = true;
-            let self = this;
-            setTimeout(function() {
-              self.$el.querySelector('.smart-search--heading').style.minHeight = 0;
-            }, 200);
-          }
-        },
-        resize: function() {
-          if (!this.isSearchBtn) {
-            let heading = this.$el.querySelector('.smart-search--heading');
-            let search = heading.querySelector('.smart-search--fieldContainer');
-            let titleSegment = heading.querySelector('.smart-search--headingSegment');
 
-            //do not add search height if screen is less than breakpoint
-            let searchHeight = document.body.clientWidth < 950 ? search.offsetHeight : 0;
-
-            heading.style.minHeight = searchHeight + titleSegment.offsetHeight + 'px';
-          }
-
-          let searchBtn = this.$el.querySelector('.smart-search--btnSearch');
-          let exitBtn = this.$el.querySelector('.smart-search--btnExit');
-          if (document.body.clientWidth < 768) {
-            searchBtn.classList.remove('bit-btn-plainSearch');
-            searchBtn.classList.add('bit-btn-search');
-
-            exitBtn.classList.remove('bit-btn-plainExit');
-            exitBtn.classList.add('bit-btn-exit');
-          } else {
-            searchBtn.classList.remove('bit-btn-search');
-            searchBtn.classList.add('bit-btn-plainSearch');
-
-            exitBtn.classList.remove('bit-btn-exit');
-            exitBtn.classList.add('bit-btn-plainExit');
-          }
-        }
-      },
-      created: function() {
-        window.addEventListener('resize', this.resize);
+  /**
+   * A component that renders a mobile oriented search bar.
+   * @author James Stanger, Washington State Patrol
+   * @version 1.0
+   */
+  export default {
+    name: "smart-search",
+    components: {
+      bitBtn,
+      bitInput
+    },
+    data() {
+      return {
+        /**
+         * Flag indicating that the search is hidden.
+         */
+        isHidden: true,
+        /**
+         * Class for the search container.
+         */
+        searchContainerClass: "smart-search--fieldContainer is-hidden"
       }
+    },
+    props: {
+      /**
+       * Corresponds to the native HTML form attribute "action"
+       */
+      action: {
+        type: String,
+        required: true
+      },
+      /**
+       * Corresponds to the native HTML form attribute "method"
+       */
+      method: {
+        type: String,
+        required: true
+      },
+      /**
+       * The title that will display to the right of the search.
+       */
+      formTitle: {
+        type: String,
+        required: true
+      }
+    },
+    methods: {
+      /**
+       * Toggles the search bar visibility.
+       */
+      toggle: function () {
+        if (this.isHidden) {
+          this.isHidden = false;
+          this.resize();
+        } else {
+          this.isHidden = true;
+          let self = this;
+          setTimeout(function () {
+            self.$el.querySelector('.smart-search--heading').style.minHeight = 0;
+          }, 200);
+        }
+      },
+      /**
+       * Event handler for the resize event. Dynamically set the height of the search container, and change
+       * the style of the toggle buttons on smaller screens.
+       */
+      resize: function () {
+        if (!this.isHidden) {
+          let heading = this.$el.querySelector('.smart-search--heading');
+          let search = heading.querySelector('.smart-search--fieldContainer');
+          let titleSegment = heading.querySelector('.smart-search--headingSegment');
+
+          //do not add search height if screen is less than breakpoint
+          let searchHeight = document.body.clientWidth < 950 ? search.offsetHeight : 0;
+
+          heading.style.minHeight = searchHeight + titleSegment.offsetHeight + 'px';
+        }
+
+        let searchBtn = this.$el.querySelector('.smart-search--btnSearch');
+        let exitBtn = this.$el.querySelector('.smart-search--btnExit');
+        if (document.body.clientWidth < 768) {
+          searchBtn.classList.remove('bit-btn-plainSearch');
+          searchBtn.classList.add('bit-btn-search');
+
+          exitBtn.classList.remove('bit-btn-plainExit');
+          exitBtn.classList.add('bit-btn-exit');
+        } else {
+          searchBtn.classList.remove('bit-btn-search');
+          searchBtn.classList.add('bit-btn-plainSearch');
+
+          exitBtn.classList.remove('bit-btn-exit');
+          exitBtn.classList.add('bit-btn-plainExit');
+        }
+      }
+    },
+    /**
+     * Sets up an event listener and the above handler for the "resize" event
+     */
+    created: function () {
+      window.addEventListener('resize', this.resize);
     }
+  }
 </script>
 
 <style scoped lang="scss">
@@ -128,7 +159,8 @@
         @include format-responsive(column, flex-start, null) {
             grid-template-columns: 100%;
             grid-auto-flow: row;
-        };
+        }
+    ;
 
         background: #d9d9d9;
         align-items: flex-end;

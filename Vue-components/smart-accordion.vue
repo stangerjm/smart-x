b552160bfc91dae89e0853683436199b4fdbdd2d
Stanger <JMST225@patrol.lcl>
2018-05-15 13:25:55 -0700

    added documentation for smart-accordion

--- Vue-components/smart-accordion.vue
+++ Vue-components/smart-accordion.vue
@@ -3,15 +3,24 @@
         <input :id="inputId" class="smart-accordion--toggle" name="accordion-1" type="checkbox" @click="toggleExpand">
         <label :for="inputId" class="smart-accordion--label">{{title}}</label>
         <article class="smart-accordion--content">
+            <!-- @slot Allows content to be passed into the expanded portion. -->
             <slot></slot>
         </article>
     </section>
 </template>
 
 <script>
+  /**
+   * A component that renders a responsive accordion that can contain any HTML content.
+   * @author James Stanger, Washington State Patrol
+   * @version 1.0
+   */
   export default {
     name: "smart-accordion",
     props: {
+      /**
+       * The title that will display before the accordion.
+       */
       title: {
         type: String,
         default: "Click to expand"
@@ -20,11 +29,14 @@
     data() {
       return {
         isExpanded: false,
-        inputId: 'ac-' + Math.random().toString(36).substr(2,9)
+        inputId: 'ac-' + Math.random().toString(36).substr(2, 9)
       }
     },
     methods: {
-      toggleExpand: function() {
+      /**
+       * Toggles the accordion to expand or collapse.
+       */
+      toggleExpand: function () {
         if (!this.isExpanded) {
           this.expandData();
           this.isExpanded = true;
@@ -33,30 +45,43 @@
           this.isExpanded = false;
         }
       },
-      expandData: function() {
+      /**
+       * Expands the accordion relative to the combined height of each of its children.
+       */
+      expandData: function () {
         let contentContainer = this.$el.querySelector(".smart-accordion--content");
         let totalHeight = 0;
 
-        for(let contentItem of contentContainer.childNodes) {
+        //loop through each child and add the height of each
+        for (let contentItem of contentContainer.childNodes) {
           let itemHeight = contentItem.offsetHeight;
-          if(itemHeight) {
+          if (itemHeight) {
             totalHeight += itemHeight;
           }
         }
 
         contentContainer.style.height = totalHeight + 'px';
       },
-      collapseData: function() {
+      /**
+       * Collapses the accordion by setting the container to have no height.
+       */
+      collapseData: function () {
         let contentContainer = this.$el.querySelector(".smart-accordion--content");
         contentContainer.style.height = 0 + 'px';
       },
-      handleResize: function() {
+      /**
+       * Event handler that will continually resize the container if it is expanded.
+       */
+      handleResize: function () {
         if (this.isExpanded) {
           this.expandData();
         }
       }
     },
-    created: function() {
+    /**
+     * Setup an event listener on window resize to resize the UI if needed.
+     */
+    created: function () {
       window.addEventListener('resize', this.handleResize);
     }
   }

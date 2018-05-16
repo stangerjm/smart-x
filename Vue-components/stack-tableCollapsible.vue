9a64f4c6680edcbb79142db060098eccc3ad521b
Stanger <JMST225@patrol.lcl>
2018-05-16 14:59:11 -0700

    Added documentation for stack-tableCollapsible

--- Vue-components/stack-tableCollapsible.vue
+++ Vue-components/stack-tableCollapsible.vue
@@ -12,27 +12,47 @@
 <script>
   import smartTable from './smart-table'
   import smartAccordion from './smart-accordion'
+
+  /**
+   * A component that renders a smart-table inside of a smart-accordion.
+   * @author James Stanger, Washington State Patrol
+   * @version 1.0
+   * @see See [smart-table](#smarttable) for more info about defaultContext and tableData props
+   * @see See [smart-accordion](#smartaccordion)
+   */
   export default {
-    name: "stack-tableCollapsible",
+    name: "stack-table-collapsible",
     components: {
       smartTable,
       smartAccordion
     },
     props: {
+      /**
+       * Data that will be passed to the smart-table.
+       */
       tableData: {
         type: Array,
         required: true
       },
+      /**
+       * The default context to set for the smart-table.
+       */
       defaultContext: {
         type: String,
         required: true
       },
+      /**
+       * Title for the smart-accordion.
+       */
       title: {
         type: String,
         default: "Associated Data"
       }
     },
     methods: {
+      /**
+       * Event handler for smart-table's recordExpanded event.
+       */
       updateAccordionHeight: function() {
         this.$children[0].expandData();
       }

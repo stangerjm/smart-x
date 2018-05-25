e10d3d02a4d5cd5404d61406f90a3757540bfc93
Stanger <JMST225@patrol.lcl>
2018-05-25 15:12:50 -0700

    Refactored bit-dropList to conform with external styles convention.

--- Vue-components/bit-dropList.vue
+++ Vue-components/bit-dropList.vue
@@ -27,32 +27,5 @@
 </script>
 
 <style scoped lang="scss">
-    @import "../sass/variables";
-    @import "../sass/misc";
-
-    /// Sets an sublist to display beneath its parent. Will show only on hover.
-    .bit-dropList {
-        /* display beneath parent. Hide by default. Set to outer z-index layer. */
-        position: absolute;
-        padding: 0;
-        display: none;
-        width: inherit;
-        margin-top: 3px;
-        z-index: $level-9;
-        list-style: none;
-        left: 0;
-        right: 0;
-
-        li {
-            /* ensure children are not transparent */
-            background: $uniform-blue;
-            padding: 3px 0;
-            box-shadow: 0 0 3px inset;
-
-            &:hover {
-                transition: border-top-width 100ms;
-                border-top: 2px groove $patch-orange--deep;
-            }
-        }
-    }
+    @import "../sass/bit-dropList";
 </style>
